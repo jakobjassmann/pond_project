2ec92ba5ee9bf936456189229380ec45e8543ec6
jakobjassmann <jakob.assmann@uzh.ch>
2023-04-25 15:56:46 +0200

    removed the test line added in comments for Nils

--- scripts/prep/prep_climate_data.R
+++ scripts/prep/prep_climate_data.R
@@ -1,8 +1,6 @@
 # Climate data perparations for the Pond Project
 # Jakob J. Assmann jakob.assmann@uzh.ch 27 October 2022
 
-# Test line
-
 # Dependencies
 reticulate::use_condaenv("rgee")
 library(tidyverse)
