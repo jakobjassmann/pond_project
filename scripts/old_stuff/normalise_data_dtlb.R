ad11c04679d3c8f6ccc005164652d8d345ba5d72
jakobjassmann <jakob.assmann@uzh.ch>
2023-05-05 15:03:01 +0200

    moved old stuff to old stuff folder

---
+++ scripts/old_stuff/normalise_data_dtlb.R
@@ -0,0 +1,36 @@
+# Quick script to min max normalize all drone bands 
+# Jakob J. Assmann jakob.assmann@uzh.ch 04 August 2022
+
+# Dependencies
+library(terra)
+
+# Read list of raster files
+raster_files <- list.files("data/drone_time_series/dtlb/dtlb_ready/",
+                           full.names = T)
+
+# Helper function to normalize raster
+norm_raster <- function(raster_file){
+  cat("Processing:", raster_file, "\n")
+  cat("Calculating global stats...\n")
+  raster_object <- rast(raster_file)
+  bands_min <- global(raster_object, min)
+  bands_max <- global(raster_object, max)
+  cat("Normalizing Band 1 (R):\n")
+  raster_object[[1]] <- (raster_object[[1]] - bands_min[1,]) / (bands_max[1,] - bands_min[1,])
+  cat("Normalizing Band 2 (G):\n")
+  raster_object[[2]] <- (raster_object[[2]] - bands_min[2,]) / (bands_max[2,] - bands_min[2,])
+  cat("Normalizing Band 3 (B)B:\n")
+  raster_object[[3]] <- (raster_object[[3]] - bands_min[3,]) / (bands_max[3,] - bands_min[3,])
+  cat("Cropping to aoi extent:\n")
+  raster_object <- crop(raster_object, vect("data/study_aois/dtlb.shp"))
+  cat("Writing raster...\n")
+  writeRaster(raster_object, 
+              gsub(".*/(.*_[0-9]{4}).*", 
+                   "data/drone_time_series/dtlb/dtlb_norm/\\1_norm.tif", 
+                   raster_file),
+              overwrite = T)
+  cat("Done.\n")
+  return(NULL)
+}
+
+map(raster_files, norm_raster)
