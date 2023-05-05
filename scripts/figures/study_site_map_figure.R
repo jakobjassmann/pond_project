ad68f4b02fe5447755866e7a5d2657187b32fa9e
jakobjassmann <jakob.assmann@uzh.ch>
2023-05-05 15:02:23 +0200

    updated study map script with new site locations

--- scripts/figures/study_site_map_figure.R
+++ scripts/figures/study_site_map_figure.R
@@ -10,8 +10,12 @@ library(cowplot)
 library(rnaturalearth)
 
 # Load study site outline(s)
-cbh_poly <- read_sf("data/study_aois/cbh.shp")
+cbh_poly <- read_sf("data/drone_time_series/cbh_timeseries/cbh_study_aoi.shp")
 cbh_centroid <- st_centroid(cbh_poly)
+tlb_poly <- read_sf("data/drone_time_series/tlb_timeseries/tlb_study_aoi.shp")
+tlb_centroid <- st_centroid(tlb_poly)
+rdg_poly <- read_sf("data/drone_time_series/rdg_timeseries/rdg_study_aoi.shp")
+rdg_centroid <- st_centroid(rdg_poly)
 
 ## Generate pan-Arctic overview map
 
@@ -57,7 +61,7 @@ pan_arctic_overview <- ggplot() +
 ## Plot overview of study area
 
 # Load Kytalyk world view imagery from 2022
-kytalyk_wv <- rast("C:/Users/jakob/Desktop/Kytalyk_WV_2022_working_copy/015284154010_01/015284154010_01_P001_MUL/22JUL21020532-M2AS-015284154010_01_P001.TIF")
+kytalyk_wv <- rast("Z:/Schaepman/Gr_Schaepman/Kytalyk/Kytalyk_2022_WorldView_Imagery/40cm_original/second_processing/015284154010_01/015284154010_01_P001_MUL/22JUL21020532-M2AS-015284154010_01_P001.TIF")
 
 # Crop imagery
 kytalyk_wv_crop <- kytalyk_wv %>%
@@ -70,22 +74,78 @@ kytalyk_wv_crop <- kytalyk_wv %>%
 # Stretch RGB values
 kytalyk_wv_crop <- kytalyk_wv_crop %>%
   stretch(minq = 0.05,
-          maxq = 0.95)
+          maxq = 0.98)
 
 # Plot using ggplot
 kytalyk_wv_plot <- ggplot() +
   geom_spatraster_rgb(data = kytalyk_wv_crop,
                       r = 1,
                       g = 2,
-                      b = 3) +
+                      b = 3,
+                      alpha = 0.8) +
   geom_sf(data=cbh_poly,
           fill = NA,
-          colour = "#FEDE00",
-          size = 1.5) +
+          colour = "#1E1D40",
+          linewidth = 2) +
+  geom_sf(
+    data = tlb_poly,
+    fill = NA,
+    colour = "#BF625A",
+    linewidth = 2) +
+  geom_sf(data=rdg_poly,
+          fill = NA,
+          colour = "#F2A922",
+          linewidth = 2) +
+  annotate("segment",
+    x = ext(kytalyk_wv_crop)[2] - 0.15 * (ext(kytalyk_wv_crop)[2] - ext(kytalyk_wv_crop)[1]), 
+    xend =  ext(kytalyk_wv_crop)[2] - 0.15 * (ext(kytalyk_wv_crop)[2] - ext(kytalyk_wv_crop)[1]) + 500, 
+    y = ext(kytalyk_wv_crop)[3] + 0.1 * (ext(kytalyk_wv_crop)[4] - ext(kytalyk_wv_crop)[3]),
+    yend = ext(kytalyk_wv_crop)[3] + 0.1 * (ext(kytalyk_wv_crop)[4] - ext(kytalyk_wv_crop)[3]),
+    linewidth = 2,
+    colour = "black"
+  ) +
+  annotate("text",
+    x = ext(kytalyk_wv_crop)[2] - 0.15 * (ext(kytalyk_wv_crop)[2] - ext(kytalyk_wv_crop)[1]) + 250,
+    y = ext(kytalyk_wv_crop)[3] + 0.15 * (ext(kytalyk_wv_crop)[4] - ext(kytalyk_wv_crop)[3]),
+    label = "500 m",
+    colour = "black",
+    fontface = "bold",
+    size = 5
+  ) +
+    annotate("text",
+      x = st_bbox(cbh_poly)[1],
+      y = st_bbox(cbh_poly)[4],
+      label = "CBH",
+      fontface = "bold",
+      hjust = 0,
+      vjust = -0.5,
+      size = 5,
+      colour = "#1E1D40"
+    ) +
+  annotate("text",
+    x = st_bbox(tlb_poly)[1],
+    y = st_bbox(tlb_poly)[4],
+    label = "TLB",
+    fontface = "bold",
+    hjust = 0,
+    vjust = -0.5,
+    size = 5,
+    colour = "#BF625A"
+  ) +
+  annotate("text",
+      x = st_bbox(rdg_poly)[1],
+      y = st_bbox(rdg_poly)[4],
+      label = "RDG",
+      fontface = "bold",
+      hjust = 0,
+      vjust = -0.5,
+      size = 5,
+      colour = "#F2A922"
+    ) +
   scale_x_continuous(expand = c(0,0)) +
   scale_y_continuous(expand = c(0,0)) +
   theme_nothing()
-
+kytalyk_wv_plot
 
 ## Combine WV plot with overlay map
 kytalyk_plot_combined <-
@@ -100,4 +160,5 @@ kytalyk_plot_combined <-
 # Export plot
 save_plot("figures/overview_map.png",
           kytalyk_plot_combined,
+          bg = "white",
           base_asp = 16/6)
