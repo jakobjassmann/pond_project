845fd0b8efb7140e0740e92de0d9c0590d4ad01b
jakobjassmann <jakob.assmann@uzh.ch>
2023-05-11 15:38:04 +0200

    updated predictions using training data from all sites

--- scripts/analysis/predict_water.R
+++ scripts/analysis/predict_water.R
@@ -1,5 +1,5 @@
-# Quick scrip to prep some training data
-# Jakob J. Assmann jakob.assmann@uzh.ch 4 August 2022
+# Script to generate training data, evaluate seperability and generate predictions
+# Jakob J. Assmann jakob.assmann@uzh.ch 11 May 2023
 
 library(terra)
 library(tidyverse)
@@ -14,84 +14,104 @@ library(dplyr)
 library(purrr)
 library(parallel)
 library(pbapply)
+
 # Load training polys from CBG
 cbh_polys <- read_sf("data/training/cbh_two_class_polys.shp")
+tlb_polys <- read_sf("data/training/tlb_two_class_polys.shp") %>%
+  mutate(., id = 1:nrow(.))
+rdg_polys <- read_sf("data/training/rdg_two_class_polys.shp") %>%
+  mutate(., id = 1:nrow(.))
 
 # Load list of raster files 
-raster_files_norm <- list.files("data/drone_time_series/cbh_timeseries/norm/",
+raster_files_cbh <- list.files("data/drone_time_series/cbh_timeseries/norm/",
                            full.names = T)
-#raster_files_focal_mean <- list.files("data/drone_time_series/cbh/cbh_focal_mean/",
-#                                full.names = T)
-#raster_files_focal_sd <- list.files("data/drone_time_series/cbh/cbh_focal_sd/",
-#                                      full.names = T)
+raster_files_tlb <- list.files("data/drone_time_series/tlb_timeseries/norm/",
+                           full.names = T)
+raster_files_rdg <- list.files("data/drone_time_series/rdg_timeseries/norm/",
+                           full.names = T) %>%
+                           .[!(grepl("2016", .) | grepl("2019_b", .))]
+raster_files_all <- c(raster_files_cbh, raster_files_tlb, raster_files_rdg)
 
-# Set years on record
-years <- c("2014_byte", "2016", "2017", "2018", "2019", "2019_b", "2020", "2021")
-
-# Write quick helper function to grab training pixels for each year
-get_training_vals <- function(year_interest){
-  cat("Extracting", year_interest, "\n")
+# Write quick helper function to grab training pixels for each year and site
+get_training_vals <- function(raster_file){
+  # Status
+  cat("\nExtracting", raster_file, "\n")
+  
   # Load raster
-  norm_raster <- rast(raster_files_norm[grepl(paste0(".*", year_interest, "\\.tif"), raster_files_norm)])
+  norm_raster <- rast(raster_file)
+  
+  # Get year
+  year_interest <- gsub(".*([0-9]{4}).*","\\1", raster_file)
+  
+  # Get site
+  site_interest <- gsub(".*(cbh|tlb|rdg).*", "\\1", raster_file)
+  
   # Subset polys for year
-  polys <- cbh_polys[pull(cbh_polys, paste0(substr(year_interest, 1, 4))) == 1, ]
+  polys <- get(paste0(site_interest, "_polys"))
+  polys <- polys[pull(polys, paste0(year_interest)) == 1, ]
   polys$ID <- 1:nrow(polys)
+  
   # Extract training data
   training_vals_norm <- terra::extract(norm_raster, vect(polys))[,1:4]
+  
   # Adjust column names of data frame
   colnames(training_vals_norm) <- c("ID", "R", "G", "B")
-  # Combine training data with metadata (incl. class info) of the polys 
-  final_training <- polys %>% st_drop_geometry() %>%
+  
+  # Combine training data with metadata (incl. class info) of the polys
+  final_training <- polys %>%
+    st_drop_geometry() %>%
     select(ID, id, class) %>%
     full_join(training_vals_norm) %>%
-        select(-ID) %>% 
-    mutate(year = year_interest)
+    select(-ID) %>%
+    mutate(year = year_interest) %>%
+    mutate(site = site_interest)
+  
   # Return training dataset for year
   return(final_training)
 } 
 # Map function over years, re-arrange and turn class vector into a factor
-cbh_training <- map(years, get_training_vals) %>% bind_rows()
-cbh_training <- arrange(cbh_training, class)
-cbh_training$class <- as.factor(cbh_training$class)
+training_all <- pblapply(raster_files_all, get_training_vals) %>% bind_rows()
+training_all <- arrange(training_all, class)
+training_all$class <- as.factor(training_all$class)
 
 # Balance the dataset by random subsetting
 set.seed(29)
-cbh_training_all <- cbh_training
-cbh_training <- cbh_training %>% group_by(year, class) %>%
+training_original <- training_all
+training_all <- training_all %>% group_by(year, class) %>%
   na.omit() %>%
   sample_n(10000)
 
 # Add gcc and bcc to data frame
-cbh_training <- cbh_training %>%
+training_all <- training_all %>%
   mutate(gcc = G / (R + G + B),
          bcc = B / (R + G + B),
          rcc = R / (R + G + B)) %>%
   na.omit()
 
 # Calculate separability
-sep_stats <- bind_rows(separability(filter(cbh_training, class == "water") %>% ungroup() %>% select(R), 
-                                    filter(cbh_training, class == "other") %>% ungroup() %>% select(R)) %>%
+sep_stats <- bind_rows(separability(filter(training_all, class == "water") %>% ungroup() %>% select(R), 
+                                    filter(training_all, class == "other") %>% ungroup() %>% select(R)) %>%
                          select(-R, -R.1),
-                       separability(filter(cbh_training, class == "water") %>% ungroup() %>% select(G), 
-                                    filter(cbh_training, class == "other") %>% ungroup() %>% select(G)) %>%
+                       separability(filter(training_all, class == "water") %>% ungroup() %>% select(G), 
+                                    filter(training_all, class == "other") %>% ungroup() %>% select(G)) %>%
                          select(-G, -G.1),
-                       separability(filter(cbh_training, class == "water") %>% ungroup() %>% select(B), 
-                                    filter(cbh_training, class == "other") %>% ungroup() %>% select(B)) %>%
+                       separability(filter(training_all, class == "water") %>% ungroup() %>% select(B), 
+                                    filter(training_all, class == "other") %>% ungroup() %>% select(B)) %>%
                          select(-B, -B.1),
-                       separability(filter(cbh_training, class == "water") %>% ungroup() %>% select(rcc), 
-                                    filter(cbh_training, class == "other") %>% ungroup() %>% select(rcc)) %>%
+                       separability(filter(training_all, class == "water") %>% ungroup() %>% select(rcc), 
+                                    filter(training_all, class == "other") %>% ungroup() %>% select(rcc)) %>%
                          select(-rcc, -rcc.1),
-                       separability(filter(cbh_training, class == "water") %>% ungroup() %>% select(gcc), 
-                                    filter(cbh_training, class == "other") %>% ungroup() %>% select(gcc)) %>%
+                       separability(filter(training_all, class == "water") %>% ungroup() %>% select(gcc), 
+                                    filter(training_all, class == "other") %>% ungroup() %>% select(gcc)) %>%
                          select(-gcc, -gcc.1),
-                       separability(filter(cbh_training, class == "water") %>% ungroup() %>% select(bcc), 
-                                    filter(cbh_training, class == "other") %>% ungroup() %>% select(bcc)) %>%
+                       separability(filter(training_all, class == "water") %>% ungroup() %>% select(bcc), 
+                                    filter(training_all, class == "other") %>% ungroup() %>% select(bcc)) %>%
                          select(-bcc, -bcc.1),)
 sep_stats$band <- row.names(sep_stats)
 
 # Look at group and variable relationships
 plot_grid(
-  ggplot(cbh_training) +
+  ggplot(training_all) +
     geom_violin(aes(x = class, y = R, fill = class)) +
     geom_text(data = sep_stats %>% filter(band == "R"), 
               aes(x = 2.5, y = 65535, 
@@ -102,7 +122,7 @@ plot_grid(
     scale_y_continuous(limits = c(0,65535)) +
     theme_cowplot() +
     theme(legend.position = "none"),
-  ggplot(cbh_training) +
+  ggplot(training_all) +
     geom_violin(aes(x = class, y = G, fill = class)) +
     geom_text(data = sep_stats %>% filter(band == "G"), 
               aes(x = 2.5, y = 65535, 
@@ -112,7 +132,7 @@ plot_grid(
     scale_y_continuous(limits = c(0,65535)) +
     theme_cowplot() +
     theme(legend.position = "none"),
-  ggplot(cbh_training) +
+  ggplot(training_all) +
     geom_violin(aes(x = class, y = B, fill = class)) +
     geom_text(data = sep_stats %>% filter(band == "B"), 
               aes(x = 2.5, y = 65535, 
@@ -122,7 +142,7 @@ plot_grid(
     scale_y_continuous(limits = c(0,65535)) +
     theme_cowplot() +
     theme(legend.position = "none"),
-  ggplot(cbh_training) +
+  ggplot(training_all) +
     geom_violin(aes(x = class, y = rcc, fill = class)) +
     geom_text(data = sep_stats %>% filter(band == "rcc"), 
               aes(x = 2.5, y = 1, 
@@ -133,7 +153,7 @@ plot_grid(
     scale_y_continuous(limits = c(0,1)) +
     theme_cowplot() +
     theme(legend.position = "none"),
-  ggplot(cbh_training) +
+  ggplot(training_all) +
     geom_violin(aes(x = class, y = gcc, fill = class)) +
     geom_text(data = sep_stats %>% filter(band == "gcc"), 
               aes(x = 2.5, y = 1, 
@@ -143,7 +163,7 @@ plot_grid(
     scale_y_continuous(limits = c(0,1)) +
     theme_cowplot() +
     theme(legend.position = "none"),
-  ggplot(cbh_training) +
+  ggplot(training_all) +
     geom_violin(aes(x = class, y = bcc, fill = class)) +
     geom_text(data = sep_stats %>% filter(band == "bcc"), 
               aes(x = 2.5, y = 1, 
@@ -163,9 +183,9 @@ plot_grid(
     base_asp = 1.2,
     bg = "white")
 
-### Plot seperability between bands and years 
+### Plot seperability between bands, years
 plot_sep_band <- function(band = "R"){
-  sep_year <- cbh_training %>% 
+  sep_year <- training_all %>% 
     ungroup() %>% 
     split(.$year) %>% 
     map(function(x){
@@ -174,10 +194,10 @@ plot_sep_band <- function(band = "R"){
         mutate(year = unique(x$year))
     }) %>% bind_rows()
   
-  cbh_training$band_to_plot <- pull(cbh_training, !!band)
-  if(band %in% c("R", "G", "B")) cbh_training$band_to_plot <- cbh_training$band_to_plot / 65535
+  training_all$band_to_plot <- pull(training_all, !!band)
+  if(band %in% c("R", "G", "B")) training_all$band_to_plot <- training_all$band_to_plot / 65535
   save_plot(paste0("figures/sepparation_", band, "_year.png"),
-            ggplot(cbh_training) +
+            ggplot(training_all) +
               geom_violin(aes(x = class, y = band_to_plot, fill = class)) +
               geom_text(data = sep_year, 
                         aes(x = 2.5, y = 1, 
@@ -199,11 +219,11 @@ plot_sep_band <- function(band = "R"){
 c("R", "G", "B", "rcc", "gcc", "bcc") %>% map(plot_sep_band)
 
 # Split into training and validation
-cbh_training$id <- 1:nrow(cbh_training)
-training <- cbh_training %>%
+training_all$id <- 1:nrow(training_all)
+training <- training_all %>%
   group_by(year, class) %>%
   slice_sample(prop = 0.8)
-validation <- filter(cbh_training, !(id %in% training$id)) 
+validation <- filter(training_all, !(id %in% training$id)) 
 
 
 # Train random forest model
@@ -213,7 +233,12 @@ rf_fit <- randomForest(class ~ rcc + bcc + R,
 # Validate on test set
 test_preds <- predict(rf_fit, newdata = validation,
                       type = "response")
-confusionMatrix(data = test_preds, validation$class)
+file_connection <- file("tables/rf_confusion_matrix.txt")
+confusionMatrix(data = test_preds, validation$class) %>%
+  print() %>%
+  capture.output() %>%
+  writeLines(file_connection)
+close(file_connection)
 
 # Accuracy per year
 acc_per_year <- function(year_interest){
@@ -223,9 +248,31 @@ acc_per_year <- function(year_interest){
   data.frame(
     year = year_interest,
     accuracy = confusionMatrix(data = test_preds, validation_data$class)$overall[1],
-    nsample = nrow(validation_data))
+    nsample_water = nrow(validation_data %>% filter(class == "water")),
+    nsample_other = nrow(validation_data %>% filter(class == "other")))
 }
-map(years, acc_per_year) %>% bind_rows()
+map(unique(validation$year), acc_per_year) %>%
+  bind_rows() %>%
+  remove_rownames() %>%
+  print() %>%
+  write_csv("tables/rf_acc_per_year.csv")
+
+# Accuracy by site
+acc_per_site <- function(site_interest){
+  validation_data <- validation %>% filter(site == site_interest)
+  test_preds <- predict(rf_fit, newdata = validation_data,
+                        type = "response")
+  data.frame(
+    site = site_interest,
+    accuracy = confusionMatrix(data = test_preds, validation_data$class)$overall[1],
+    nsample_water = nrow(validation_data %>% filter(class == "water")),
+    nsample_other = nrow(validation_data %>% filter(class == "other")))
+}
+map(unique(validation$site), acc_per_site) %>%
+  bind_rows() %>%
+  remove_rownames() %>%
+  print() %>%
+  write_csv("tables/rf_acc_per_site.csv")
 
 # Save the model
 save(rf_fit, file = "data/models/cbh_rf_may2023.Rda")
@@ -237,6 +284,7 @@ load("data/models/cbh_rf_may2023.Rda")
 dir.create("data/drone_time_series/cbh_timeseries/preds/")
 dir.create("data/drone_time_series/tlb_timeseries/preds/")
 dir.create("data/drone_time_series/rdg_timeseries/preds/")
+
 preds_rasters <- map(c("cbh", "tlb", "rdg"), function(site_interest) {
     site_raster_files <- list.files(paste0("data/drone_time_series/", site_interest, "_timeseries/norm/"), full.names = TRUE)
     map(site_raster_files, function(rast_file) {
