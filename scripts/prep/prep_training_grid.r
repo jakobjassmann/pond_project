1994d8191fd4e4e40982b56549d014f6a6b9316a
jakobjassmann <jakob.assmann@uzh.ch>
2023-10-04 15:42:44 +0200

    updated training and model (cbh only)

--- scripts/prep/prep_training_grid.r
+++ scripts/prep/prep_training_grid.r
@@ -26,8 +26,8 @@ cat(
 )
 
 # All sites are roughly 500 m x 400 m
-# => Realistically, I can do 100 annotations per year.
-# that means a 50 m spacing in x and a 40 m spacing in y
+# => Reallistically I can do about 25 annotaions per year
+# => 100 m x 100 m cells
 
 # Generate polygons grid CBH
 cbh_centroid <- st_centroid(cbh) %>% st_coordinates()
@@ -35,7 +35,7 @@ cbh_grid_ext <- ext(
     (cbh_centroid[1] - 25) - 5 * 50, (cbh_centroid[1] + 25) + 5 * 50,
     (cbh_centroid[2] - 25) - 5 * 50, (cbh_centroid[2] + 25) + 5 * 50
 )
-cbh_grid <- rast(cbh_grid_ext, nrows = 10, ncols = 10, crs = crs(cbh)) %>%
+cbh_grid <- rast(cbh_grid_ext, nrows = 5, ncols = 5, crs = crs(cbh)) %>%
     as.polygons() %>%
     st_as_sf()
 plot(cbh_grid)
@@ -48,7 +48,7 @@ tlb_grid_ext <- ext(
     (tlb_centroid[1] - 25) - 5 * 50, (tlb_centroid[1] + 25) + 5 * 50,
     (tlb_centroid[2] - 25) - 5 * 50, (tlb_centroid[2] + 25) + 5 * 50
 )
-tlb_grid <- rast(tlb_grid_ext, nrows = 10, ncols = 10, crs = crs(tlb)) %>%
+tlb_grid <- rast(tlb_grid_ext, nrows = 5, ncols = 5, crs = crs(tlb)) %>%
     as.polygons() %>%
     st_as_sf()
 plot(tlb_grid)
@@ -61,9 +61,30 @@ rdg_grid_ext <- ext(
     (rdg_centroid[1] - 25) - 5 * 50, (rdg_centroid[1] + 25) + 5 * 50,
     (rdg_centroid[2] - 25) - 5 * 50, (rdg_centroid[2] + 25) + 5 * 50
 )
-rdg_grid <- rast(rdg_grid_ext, nrows = 10, ncols = 10, crs = crs(rdg)) %>%
+rdg_grid <- rast(rdg_grid_ext, nrows = 5, ncols = 5, crs = crs(rdg)) %>%
     as.polygons() %>%
     st_as_sf()
 plot(rdg_grid)
 plot(rdg, add = T, col = "red")
 write_sf(rdg_grid, "data/training/rdg_grid.gpkg")
+
+# The plan will be to mark all cells for 2014 (in space)
+# Then mark 3 cells per year.
+# Here we generate the coordinates of these:
+years <- c(2014,2016:2021)
+sites <- c("cbh", "tlb", "rdg")
+set.seed(21)
+sample_combos <- map(sites, function(site) {
+    map2(years, site, function(y, site) {
+        data.frame(site = site, year = y, cells = sample(1:25, 3))
+    })
+}) %>% bind_rows()
+write_csv(sample_combos, "data/training/grid_cells_to_annotate.csv")
+# Cell numbering starting top-left
+
+# generate some extra data for the outlier sites
+# CHH 2019_b
+sample(1:25, 3)
+
+# too little sample data in 2016 -> generate one more random cell
+sample(1:25,1)
