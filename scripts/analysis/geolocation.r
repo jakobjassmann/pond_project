dd71ee160d27ce270239a003c364cf22cde96fe8
jakobjassmann <jakob.assmann@uzh.ch>
2024-03-09 13:51:38 +0100

    updated geolocation figures

--- scripts/analysis/geolocation.r
+++ scripts/analysis/geolocation.r
@@ -8,15 +8,15 @@ library(cowplot)
 library(tidyverse)
 library(tidyterra)
 library(terra)
-library(tidyterra)
 
 # Load test point annotations
-cbh_test_points <- st_read("data/drone_time_series/cbh_timeseries/cbh_test_points.shp")
-tlb_test_points <- st_read("data/drone_time_series/tlb_timeseries/tlb_test_points.shp")
+cbh_test_points <- st_read("data/drone_data/cbh/cbh_test_points.shp") %>%
+    mutate(name = gsub("text", "test", name))
+tlb_test_points <- st_read("data/drone_data/tlb/tlb_test_points.shp")
 tlb_test_points <- tlb_test_points %>%
     mutate(year = yeat) %>%
     select(-yeat)
-rdg_test_points <- st_read("data/drone_time_series/rdg_timeseries/rdg_test_points.shp") %>%
+rdg_test_points <- st_read("data/drone_data/rdg/rdg_test_points.shp") %>%
     st_transform(crs = st_crs(cbh_test_points))
 
 # Check completness
@@ -357,36 +357,40 @@ lapply(., function(x){
             ) +
         geom_hline(yintercept = 3 * 0.12, linetype = "dashed") +
             scale_y_continuous(limits = c(0, 2.5)) +
+            annotate("text", 
+            x = -Inf, 
+            y = 3 * 0.12 + 0.15, 
+            label = "  3x GSD",
+            hjust = 0) +
             theme_cowplot()
-    if (unique(x$site) == "rdg") {
-        site_plot <- site_plot + annotate("text", x = length(unique(x$year)), y = 3 * 0.12 + 0.15, label = "3x GSD")
-    }
     return(site_plot)
 })
 # Arrange grid and save
 plot_grid(geolocation_per_site[[1]],
 geolocation_per_site[[3]],
 geolocation_per_site[[2]],
- labels = c("a)", "b)", "c)"), nrow = 1) %>%
+ labels = c("a)", "b)", "c)"), nrow = 3, ncol = 1) %>%
     save_plot("figures/geolocation_accuracy.png", .,
         base_asp = 1.6,
-    ncol = 3,    bg = "white"
+        ncol = 1,
+        nrow = 3,    
+        bg = "white"
     )
 
 
 # Quick plots that illustrate the marking process
 # Pick on random testpoint form each site
-points_to_plot <- bind_rows(
-    cbh_test_points,
-    tlb_test_points,
-    rdg_test_points
-) %>%
-    mutate(year = case_when(year == "2019" ~ "2019_a", TRUE ~ year)) %>%
-    mutate(site = gsub("(^[a-z]{3}).*", "\\1", name)) %>%
-    group_by(site) %>%
-        sample_n(1) %>% pull(name)
+#points_to_plot <- bind_rows(
+#    cbh_test_points,
+#    tlb_test_points,
+#    rdg_test_points
+#) %>%
+#    mutate(year = case_when(year == "2019" ~ "2019_a", TRUE ~ year)) %>%
+#    mutate(site = gsub("(^[a-z]{3}).*", "\\1", name)) %>%
+#    group_by(site) %>%
+#        sample_n(1) %>% pull(name)
 
-# Helper function to plot point and time_series
+# Helper function to plot point on drone data crop - time_series
 plot_point <- function(point_name) {
     # Get all point coordinates
     point_coordinates <- bind_rows(
@@ -398,13 +402,13 @@ plot_point <- function(point_name) {
     # Get site name
     site_name <- gsub("^([a-z]{3}).*", "\\1", point_name)
     # Load rasters
-    drone_rast <- list.files(paste0("data/drone_time_series/", site_name, "_timeseries/norm"),
+    drone_rast <- list.files(paste0("data/drone_data/", site_name, "/norm"),
         pattern = "tif",
         full.names = T
     ) %>% lapply(rast)
-    # Get 2021 plot and derive extent (5 m buffer)
+    # Get 2021 plot and derive extent (3 m buffer)
     point_2021 <- filter(point_coordinates, grepl("2021", year))
-    point_2021_buffered <- st_buffer(point_2021, 5)
+    point_2021_buffered <- st_buffer(point_2021, 3)
     # Set aoi extent
     aoi_ext <- ext(
         st_bbox(point_2021_buffered)[1],
@@ -427,7 +431,12 @@ plot_point <- function(point_name) {
                     colour = "magenta",
                     size = 5
                 ) +
-                theme_nothing()
+                labs(title = point_coordinates$year[x], ylab = "3 m", xlab = "3 m") +
+                theme_map() +
+                theme(
+                    axis.title.x = element_text(),
+                    axis.title.y = element_text(angle = -90),
+                    plot.title = element_text(hjust = 0.5))
         }) %>%
         plot_grid(plotlist = ., nrow = 1)
     # return plot
@@ -435,9 +444,58 @@ plot_point <- function(point_name) {
 }
 
 # Generate plots
-point_plots <- lapply(points_to_plot, plot_point) 
+point_plots_cbh <- cbh_test_points %>%
+    pull(name) %>%
+    unique() %>%
+    lapply(plot_point)
+point_plots_rdg <- rdg_test_points %>%
+    pull(name) %>%
+    unique() %>%
+    lapply(plot_point)
+point_plots_tlb <- tlb_test_points %>%
+    pull(name) %>%
+    unique() %>%
+    lapply(plot_point)
 
 # Save plots
-point_plots %>%
-    plot_grid(plotlist = ., nrow = 3) %>%
-    save_plot("figures/geolocation_test_plots_examples.png", ., base_asp = 9, nrow = 3)
+point_plots_cbh %>%
+    plot_grid(plotlist = ., 
+    nrow = 5,
+    labels = paste0(letters[1:5], ") Point ", cbh_test_points %>%
+        pull(name) %>%
+        unique() %>% gsub("cbh_test_point", "", .)),
+        label_size = 18,
+    hjust = 0,
+    vjust = 1) %>%
+    save_plot("figures/cbh/geolocation_test_points_cbh.png", .,
+        base_asp = 9, nrow = 5,
+        bg = "white"
+    )
+point_plots_rdg %>%
+    plot_grid(
+        plotlist = .,
+        nrow = 5,
+        labels = paste0(letters[1:5], ") Point ", rdg_test_points %>%
+            pull(name) %>%
+            unique() %>% gsub("rdg_test_point", "", .)),
+        label_size = 18,
+        hjust = 0,
+        vjust = 1
+    ) %>%
+    save_plot("figures/rdg/geolocation_test_points_rdg.png", .,
+        base_asp = 9, nrow = 5,
+        bg = "white"
+    )
+point_plots_tlb %>%
+    plot_grid(plotlist = ., 
+    nrow = 5,
+    labels = paste0(letters[1:5], ") Point ", tlb_test_points %>%
+        pull(name) %>%
+        unique() %>% gsub("tlb_test_point", "", .)),
+    label_size = 18,
+    hjust = 0,
+    vjust = 1) %>%
+    save_plot("figures/tlb/geolocation_test_points_tlb.png", .,
+        base_asp = 9, nrow = 5,
+        bg = "white"
+    )
