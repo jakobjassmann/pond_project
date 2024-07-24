500be77cf7057b4fc0cfed5c4c1fd93d8909378b
jakobjassmann <jakob.assmann@uzh.ch>
2024-07-24 16:59:35 +0200

    Updated climate water area correlations -> now at site level

--- scripts/analysis/climate_analysis.R
+++ scripts/analysis/climate_analysis.R
@@ -30,42 +30,81 @@ water_climate %>% relocate(site, year) %>%
 # Test correlations
 correlations <- map(names(water_climate)[c(-1, -2, -3)],
     function(x){
+      water_climate %>%
+        split(.$site) %>%
+        imap(function(y, z){
       data.frame(
+        site = z,
         var = x,
-        r = cor(pull(water_climate, prop) , pull(water_climate, !!x), method = "pearson"),
-        p = cor.test(pull(water_climate, prop) , pull(water_climate, !!x), method = "pearson")$p.value)
+        r = cor(pull(y, prop) , pull(y, !!x), method = "pearson"),
+        p = cor.test(pull(y, prop) , pull(y, !!x), method = "pearson")$p.value)
+        }) %>%
+        bind_rows()
     }) %>% bind_rows() %>%
-  arrange(desc(abs(r))) %>%
+  arrange(site, desc(abs(r))) %>%
   mutate(r = round(r, 2), p = round(p, 3))
-correlations
-#                    var     r     p
-# 1        temp_jja_mean -0.42 0.118
-# 2       precip_son_sum  0.34 0.216
-# 3        temp_son_mean  0.26 0.342
-# 4       precip_jja_sum -0.26 0.351
-# 5        temp_mam_mean  0.25 0.370
-# 6       spei_12_months  0.22 0.428
-# 7       precip_dfj_sum  0.22 0.438
-# 8        spei_9_months  0.20 0.474
-# 9       spei_24_months  0.20 0.479
-# 10 precip_sept_aug_sum  0.17 0.555
-# 11       spei_6_months  0.11 0.691
-# 12   precip_annual_sum -0.09 0.748
-# 13      precip_mam_sum  0.08 0.766
-# 14       spei_3_months  0.08 0.773
-# 15       temp_djf_mean  0.05 0.854
-# 16    temp_annual_mean  0.03 0.902
+# site                 var     r     p
+# 1   cbh       temp_jja_mean -0.71 0.074
+# 2   cbh      precip_son_sum  0.56 0.192
+# 3   cbh       temp_son_mean  0.46 0.296
+# 4   cbh       temp_mam_mean  0.45 0.309
+# 5   cbh      precip_jja_sum -0.39 0.382
+# 6   cbh      spei_12_months  0.35 0.437
+# 7   cbh      precip_dfj_sum  0.34 0.462
+# 8   cbh       spei_9_months  0.33 0.466
+# 9   cbh      spei_24_months  0.31 0.505
+# 10  cbh precip_sept_aug_sum  0.28 0.548
+# 11  cbh       spei_6_months  0.20 0.665
+# 12  cbh       spei_3_months  0.16 0.731
+# 13  cbh      precip_mam_sum  0.13 0.785
+# 14  cbh   precip_annual_sum -0.12 0.800
+# 15  cbh    temp_annual_mean  0.07 0.883
+# 16  cbh       temp_djf_mean  0.06 0.898
+# 
+# 33  tlb       temp_mam_mean  0.56 0.191
+# 34  tlb       temp_jja_mean -0.55 0.199
+# 35  tlb      precip_jja_sum -0.44 0.322
+# 36  tlb   precip_annual_sum -0.42 0.350
+# 37  tlb      precip_son_sum  0.32 0.491
+# 38  tlb       temp_son_mean  0.30 0.507
+# 39  tlb      precip_dfj_sum  0.29 0.522
+# 40  tlb      spei_24_months  0.26 0.577
+# 41  tlb      spei_12_months  0.20 0.663
+# 42  tlb    temp_annual_mean  0.19 0.675
+# 43  tlb       spei_9_months  0.15 0.754
+# 44  tlb       temp_djf_mean -0.05 0.913
+# 45  tlb      precip_mam_sum  0.02 0.969
+# 46  tlb precip_sept_aug_sum  0.02 0.973
+# 47  tlb       spei_6_months  0.00 0.993
+# 48  tlb       spei_3_months  0.00 0.995
 
 # Save as table 
 write_csv(correlations, "tables/climate_water_cor.csv")
 
-# Get min and max values for summer temp and autum precipitation
-climate_data %>% 
+# Get min, max and 95% quantiles for summer temp and autum precipitation
+climate_extremes <- climate_data %>% 
   select(temp_jja_mean, precip_son_sum) %>%
   summarise(min_temp = min(temp_jja_mean),
             max_temp = max(temp_jja_mean),
             min_precip = min(precip_son_sum),
-            max_precip = max(precip_son_sum))
+            max_precip = max(precip_son_sum),
+            q0.05_temp = quantile(temp_jja_mean, 0.05),
+            q0.95_temp = quantile(temp_jja_mean, 0.95),
+            q0.05_precip = quantile(precip_son_sum, 0.05),
+            q0.95_precip = quantile(precip_son_sum, 0.95))
+# Do our time-series values exceed the extremes?
+filter(climate_data, year >= 2014, year <= 2021) %>%
+  pull(temp_jja_mean) %>%
+  {. < climate_extremes$q0.05_temp}
+filter(climate_data, year >= 2014, year <= 2021) %>%
+  pull(temp_jja_mean) %>%
+  {. > climate_extremes$q0.95_temp}
+filter(climate_data, year >= 2014, year <= 2021) %>%
+  pull(precip_son_sum) %>%
+  {. < climate_extremes$q0.05_precip}
+filter(climate_data, year >= 2014, year <= 2021) %>%
+  pull(precip_son_sum) %>%
+  {. > climate_extremes$q0.95_precip}
 
 # Plot climate data for centrury
 climate_cent <- ggplot(climate_data) +
